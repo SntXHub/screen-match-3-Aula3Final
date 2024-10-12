4281eb681acb1d99288a8190359402370d9d7513
Santiago Gabriel Cabrera <santyx@gmail.com>
2024-10-11 21:24:07 -0300

    Segundo Commit

--- src/com/alura/screenmatch/principal/Principal.java
+++ src/com/alura/screenmatch/principal/Principal.java
@@ -27,7 +27,7 @@ public class Principal {
         lost.setTemporadas(10);
         lost.setEpisodiosPorTemporada(10);
         lost.setMinutosPorEpisodio(50);
-        System.out.println("Duracion de la série: " + lost.getDuracionEnMinutos());
+        System.out.println("Duracion de la serie: " + lost.getDuracionEnMinutos());
 
         Pelicula otraPelicula = new Pelicula("Avatar", 2023);
         otraPelicula.setDuracionEnMinutos(200);
